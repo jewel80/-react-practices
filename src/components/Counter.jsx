45f6a6afc45814b7020a8ae11687020daa3034d1
Md. Jewel Rana <m.jewelrana80@gmail.com>
2022-11-05 21:47:04 +0600

    react-redux | complete counter react app

---
+++ src/components/Counter.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { decrementCounter, incrementCounter, resetCounter } from '../services/actions/counterAction';
+
+const Counter = () => {
+    const count = useSelector((state) => state.count)
+    console.log(count);
+
+    const dispatch = useDispatch();
+    const handleIncrement = () => {
+        dispatch(incrementCounter())
+    }
+    const handleDencrement = () => {
+        dispatch(decrementCounter())
+    }
+    const handleReset = () => {
+        dispatch(resetCounter())
+    }
+
+    return (
+        <div>
+            <h2>Counter App</h2>
+            <h3>Count: {count}</h3>
+            <button className='btn btn-info' onClick={handleIncrement}>Increment</button>
+            <button className='btn btn-danger' onClick={handleReset}>Reset</button>
+            <button className='btn btn-warning' onClick={handleDencrement}>Decrement</button>
+        </div>
+    )
+}
+
+export default Counter
